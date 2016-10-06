bab502d48535102ee3aac4d62c35ef3e94ef6ebf
Alisson Vieira <alisson.echo@gmail.com>
2016-10-06 00:42:36 -0300

    Tipos, Herança e Interfaces

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 
+/*
 require_once "Pessoa.php";
 
 $pessoa1 = new Pessoa("Alisson", 30);
-
 $pessoa2 = new Pessoa("Maria", 20);
 
-
-
 $pessoa1->correr(50);
+*/
+require_once "Produto.php";
+require_once "Tenis.php";
+
+$tenis = TenisAdidas();
+$tenis->getCor();
+
+$tenis2 = new SapaTenis();
+$tenis2->getCor();
+$tenis2->getTamanho();
---
+++ interfaces/ComputadorDesktop.php
@@ -0,0 +1,25 @@
+<?php
+
+class ComputadorDesktop implements ComputadorInterface
+{
+    public function getCor()
+    {
+
+    }
+
+    public function getPeso()
+    {
+
+    }
+
+    public function getTela()
+    {
+
+    }
+
+    public function getTamanho()
+    {
+        // TODO: Implement getTamanho() method.
+    }
+
+}
